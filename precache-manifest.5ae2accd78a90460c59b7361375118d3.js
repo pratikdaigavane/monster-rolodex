a343f68633f9291a679c23b2be0102d0173976d6
pratikdaigavane <daigavanep@gmail.com>
2019-12-26 22:07:26 +0530

    Updates

---
+++ precache-manifest.5ae2accd78a90460c59b7361375118d3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c2525aafc6f183308fe9949ebf3f25b",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "ed8202d649dce66dadaa",
+    "url": "/monsters-rolodex/static/css/main.47601185.chunk.css"
+  },
+  {
+    "revision": "dcd2657b3ef8f78d14f4",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-rolodex/static/js/2.e6fbc2c9.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ed8202d649dce66dadaa",
+    "url": "/monsters-rolodex/static/js/main.19cd588e.chunk.js"
+  },
+  {
+    "revision": "6e191d8baec223c78143",
+    "url": "/monsters-rolodex/static/js/runtime-main.c1ee7b67.js"
+  }
+]);
